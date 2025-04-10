4a7b98caa7a33ad7602467ea9698aba395b97a05
Zasharan2 <65794527+Zasharan2@users.noreply.github.com>
2025-04-10 12:07:44 -0400

    Update meme.js

--- meme.js
+++ meme.js
@@ -1,5 +1,5 @@
 var randomMemeImage = document.getElementById("randomMeme");
 
-var images = ["among-us-sus.gif", "baldi-dance.gif", "caipirinha.gif", "coca-cola.gif", "crying.gif", "crying-dog.jpg", "despair.gif", "jumping-cat.gif", "obamium.gif", "rick-roll.gif", "shock.jpg", "shrek-smirk.gif", "students.gif", "squidward.jpg", "triangle-dance.gif", "walter-white.gif", "yippee.gif", "zooweemama.jpg"];
+var images = ["among-us-sus.gif", "baldi-dance.gif", "caipirinha.gif", "chiikawa-eat.jpg", "coca-cola.gif", "crying.gif", "crying-dog.jpg", "despair.gif", "jumping-cat.gif", "obamium.gif", "rick-roll.gif", "shock.jpg", "shrek-smirk.gif", "students.gif", "squidward.jpg", "triangle-dance.gif", "walter-white.gif", "yippee.gif", "zooweemama.jpg"];
 
 randomMemeImage.src = "images/home/" + images[Math.floor(Math.random() * images.length)];
